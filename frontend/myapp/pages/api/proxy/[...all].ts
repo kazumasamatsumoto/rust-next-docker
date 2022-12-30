fb59c788c54288d194d186d03bf3f5c55a6a615f
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2022-12-30 11:28:43 +0900

    動かなくなった

--- frontend/myapp/pages/api/proxy/[...all].ts
+++ frontend/myapp/pages/api/proxy/[...all].ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import httpProxyMiddleware from "next-http-proxy-middleware";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (req: NextApiRequest, res: NextApiResponse) =>
   httpProxyMiddleware(req, res, {
     target: `http://host.docker.internal:8080`,
